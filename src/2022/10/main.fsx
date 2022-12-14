3ed8e65aee96113364e127541cb71b77cabf59a4
leow93 <leowheelan@hotmail.co.uk>
2022-12-14 19:29:10 +0000

    feat: day 10 part II

--- src/2022/10/main.fsx
+++ src/2022/10/main.fsx
@@ -7,35 +7,31 @@ type Instruction =
 module Instruction =
   let parse s =
     if s = "noop" then
-      [|Noop|]
+      [| Noop |]
     elif s.StartsWith "addx" then
-      [|Noop; Add(s.Substring 5 |> int)|]
+      [| Noop; Add(s.Substring 5 |> int) |]
     else
       failwithf "unknown instruction: %s" s
-  
+
 module State =
-  type State =
-    { count: int
-      history: int list }
-  let initial =
-    { count = 1
-      history = [ 1 ] }
+  type State = { count: int; history: int list }
+  let initial = { count = 1; history = [ 1 ] }
 
   let private addToHistory history x = List.append history [ x ]
-  
-  let private addCurrentCountToHistory state =
-    addToHistory state.history state.count
-    
+
+  let private addCurrentCountToHistory state = addToHistory state.history state.count
+
   let evolve state instruction =
     match instruction with
-    | Noop -> {state with history = addCurrentCountToHistory state }
+    | Noop -> { state with history = addCurrentCountToHistory state }
     | Add x ->
       let count = state.count + x
-      {count = count; history = addToHistory state.history count}
 
-let relevantCycles (i, _) =
-  i = 20 || (i-20) % 40 = 0
-let signalStrength (a, b) = a * b  
+      { count = count
+        history = addToHistory state.history count }
+
+let relevantCycles (i, _) = i = 20 || (i - 20) % 40 = 0
+let signalStrength (a, b) = a * b
 
 let partOne file =
   readLines file
@@ -43,16 +39,36 @@ let partOne file =
   |> Array.concat
   |> Array.fold State.evolve State.initial
   |> (fun state ->
-      state.history
-      |> List.indexed
-      |> List.map(fun (i, x) -> (i + 1, x))
-      |> List.take 221
-      |> List.filter relevantCycles
-      |> List.sumBy signalStrength
-    )
+    state.history
+    |> List.indexed
+    |> List.map (fun (i, x) -> (i + 1, x))
+    |> List.take 221
+    |> List.filter relevantCycles
+    |> List.sumBy signalStrength)
+
+let partTwo file =
+  readLines file
+  |> Array.map Instruction.parse
+  |> Array.concat
+  |> Array.fold State.evolve State.initial
+  |> (fun state ->
+    state.history
+    |> List.mapi (fun i spriteIdx ->
+      let position = i % 40
+
+      match abs (spriteIdx - position) < 2 with
+      | true -> '#'
+      | false -> '.'))
+  |> List.iteri (fun i char ->
+    if i % 40 = 0 then printf "\n"
+    printf "%c" char)
 
 
 partOne "./test.txt"
 |> printfn "Part I (TEST): %i"
+
 partOne "./data.txt"
 |> printfn "Part I (REAL): %i"
+
+partTwo "./test.txt"
+partTwo "./data.txt"
