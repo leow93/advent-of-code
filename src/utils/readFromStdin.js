887647f89710e2a479a26734e72d6ebaf3a273e8
leow93 <leowheelan@hotmail.co.uk>
2023-12-04 20:57:46 +0000

    docs: stdin reader js

--- src/utils/readFromStdin.js
+++ src/utils/readFromStdin.js
@@ -1,3 +1,8 @@
+/**
+ * Very useful for piping data into a script e.g.
+ * cat input.txt | node main.js
+ * @returns {Promise<string>}
+ */
 const readFromStdin = async () => {
   const chunks = [];
   for await (const chunk of process.stdin) {
