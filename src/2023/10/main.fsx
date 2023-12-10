f9046c6d8f15842efaf3fd159b8096ecf68b0264
leow93 <leowheelan@hotmail.co.uk>
2023-12-10 21:36:12 +0000

    feat: day 10 part two

--- src/2023/10/main.fsx
+++ src/2023/10/main.fsx
@@ -9,7 +9,6 @@ type Coord = int * int
 
 type Cell =
   | Empty of Coord
-  | Start of Coord
   | NS of Coord
   | EW of Coord
   | NE of Coord
@@ -17,95 +16,102 @@ type Cell =
   | SE of Coord
   | SW of Coord
 
-module Parsing =
-  let parse (lines: string array) =
-    lines
-    |> Array.mapi (fun i line ->
-      line.ToCharArray()
-      |> Array.indexed
-      |> Array.choose (fun (j, c) ->
-        let coord = i, j
-
-        match c with
-        | '.' -> Empty coord |> Some
-        | '|' -> NS coord |> Some
-        | '-' -> EW coord |> Some
-        | 'L' -> NE coord |> Some
-        | 'J' -> NW coord |> Some
-        | '7' -> SW coord |> Some
-        | 'F' -> SE coord |> Some
-        | 'S' -> Start coord |> Some
-        | _ -> None))
+type MarkedCell =
+  | LoopPipe of Cell
+  | Other of Cell
 
 let item (x, y) grid =
   grid |> Array.tryItem x |> Option.bind (fun row -> row |> Array.tryItem y)
 
-let findElement grid element =
-  let rec inner i =
-    match grid |> Array.tryItem i with
-    | None -> None
-    | Some row ->
-      match row |> Array.tryFindIndex (fun e -> e = element) with
-      | Some j -> Some(i, j)
-      | None -> inner (i + 1)
+module Parsing =
+  let private findStartCoord grid =
+    let rec inner i =
+      match grid |> Array.tryItem i with
+      | None -> None
+      | Some row ->
+        match row |> Array.tryFindIndex Option.isNone with
+        | Some j -> Some(i, j)
+        | None -> inner (i + 1)
 
-  inner 0
+    inner 0
 
-let findStart grid =
-  let rec inner i =
-    match grid |> Array.tryItem i with
-    | None -> None
-    | Some row ->
-      match
-        row
-        |> Array.tryFindIndex (fun e ->
-          match e with
-          | Start _ -> true
-          | _ -> false)
-      with
-      | Some j -> Some(i, j)
-      | None -> inner (i + 1)
+  let private inferStart start grid =
+    let x, y = start
 
-  inner 0
+    let above =
+      match item (x - 1, y) grid with
+      | Some(Some(NS x)) -> Some(NS x)
+      | Some(Some(SW x)) -> Some(SW x)
+      | Some(Some(SE x)) -> Some(SW x)
+      | _ -> None
 
-let getStartNeighbours grid curr =
-  let x, y = curr
+    let below =
+      match item (x + 1, y) grid with
+      | Some(Some(NS x)) -> Some(NS x)
+      | Some(Some(NW x)) -> Some(NW x)
+      | Some(Some(NE x)) -> Some(NE x)
+      | _ -> None
 
-  let above =
-    match item (x - 1, y) grid with
-    | Some(NS x) -> Some(NS x)
-    | Some(SW x) -> Some(SW x)
-    | Some(SE x) -> Some(SW x)
-    | _ -> None
+    let left =
+      match item (x, y - 1) grid with
+      | Some(Some(EW x)) -> Some(EW x)
+      | Some(Some(NE x)) -> Some(NE x)
+      | Some(Some(SE x)) -> Some(SE x)
+      | _ -> None
 
-  let below =
-    match item (x + 1, y) grid with
-    | Some(NS x) -> Some(NS x)
-    | Some(NW x) -> Some(NW x)
-    | Some(NE x) -> Some(NE x)
-    | _ -> None
+    let right =
+      match item (x, y + 1) grid with
+      | Some(Some(EW x)) -> Some(EW x)
+      | Some(Some(NW x)) -> Some(NW x)
+      | Some(Some(SW x)) -> Some(SW x)
+      | _ -> None
 
-  let left =
-    match item (x, y - 1) grid with
-    | Some(EW x) -> Some(EW x)
-    | Some(NE x) -> Some(NE x)
-    | Some(SE x) -> Some(SE x)
-    | _ -> None
+    match above, below, left, right with
+    | Some _, Some _, None, None -> NS start
+    | Some _, None, Some _, None -> NW start
+    | Some _, None, None, Some _ -> NE start
+    | None, Some _, Some _, None -> SW start
+    | None, Some _, None, Some _ -> SE start
+    | None, None, Some _, Some _ -> EW start
+    | _ -> failwith "Could not infer start"
 
-  let right =
-    match item (x, y + 1) grid with
-    | Some(EW x) -> Some(EW x)
-    | Some(NW x) -> Some(NW x)
-    | Some(SW x) -> Some(SW x)
-    | _ -> None
+  let parse (lines: string array) =
+    let grid =
+      lines
+      |> Array.mapi (fun i line ->
+        line.ToCharArray()
+        |> Array.indexed
+        |> Array.map (fun (j, c) ->
+          let coord = i, j
 
-  [ above; below; left; right ]
-  |> List.choose id
-  |> (fun x ->
-    if x.Length > 2 then
-      failwithf "Too many neighbours: %A" x
-    else
-      x)
+          match c with
+          | '.' -> Empty coord |> Some
+          | '|' -> NS coord |> Some
+          | '-' -> EW coord |> Some
+          | 'L' -> NE coord |> Some
+          | 'J' -> NW coord |> Some
+          | '7' -> SW coord |> Some
+          | 'F' -> SE coord |> Some
+          // Keep as None for now, we'll infer the proper start later
+          | 'S' -> None
+          | _ -> failwithf "Unknown character: %c" c))
+
+    match findStartCoord grid with
+    | None -> failwith "No start found"
+    | Some start ->
+      // here's where we infer the start
+      let startCell = inferStart start grid
+
+      let finalGrid =
+        grid
+        |> Array.map (fun row ->
+          row
+          |> Array.map (function
+            | Some x -> x
+            // startCell is the only None
+            | None -> startCell))
+
+      finalGrid, startCell
 
 let nextCells coords grid =
   coords |> List.choose (fun coord -> item coord grid)
@@ -113,7 +119,6 @@ let nextCells coords grid =
 let getNeighbours grid curr =
   match curr with
   | Empty _ -> []
-  | Start xy -> getStartNeighbours grid xy
   | NS(x, y) -> grid |> nextCells [ (x + 1, y); (x - 1, y) ]
   | EW(x, y) -> grid |> nextCells [ (x, y + 1); (x, y - 1) ]
   | NE(x, y) -> grid |> nextCells [ (x, y + 1); (x - 1, y) ]
@@ -121,36 +126,51 @@ let getNeighbours grid curr =
   | SW(x, y) -> grid |> nextCells [ (x, y - 1); (x + 1, y) ]
   | NW(x, y) -> grid |> nextCells [ (x, y - 1); (x - 1, y) ]
 
-let countLoopSize grid =
-  let start = findStart grid
+let walkTheLoop (grid, start) =
+  let mutable count = 0
+  let queue = Queue<Cell>()
+  queue.Enqueue(start)
+  let mutable visited: Set<Cell> = Set.ofList [ start ]
 
-  match start with
-  | None -> failwith "No start found"
-  | Some start ->
-    let mutable count = 0
-    let queue = Queue<Cell>()
-    queue.Enqueue(Start start)
-    let mutable visited: Set<Cell> = Set.ofList [ Start start ]
+  while queue.Count > 0 do
+    let curr = queue.Dequeue()
+    let neighbours = getNeighbours grid curr
 
-    while queue.Count > 0 do
-      let curr = queue.Dequeue()
-      let neighbours = getNeighbours grid curr
+    for n in neighbours do
+      if not (visited |> Set.contains n) then
+        visited <- visited |> Set.add n
+        queue.Enqueue n
+        count <- count + 1
 
-      // printfn "curr %A" curr
-      // printfn "Neighbours %A" neighbours
+  let markedGrid =
+    grid
+    |> Array.map (fun row ->
+      row
+      |> Array.map (fun cell ->
+        if visited |> Set.contains cell then
+          LoopPipe cell
+        else
+          Other cell))
 
-      for n in neighbours do
-        if not (visited |> Set.contains n) then
-          visited <- visited |> Set.add n
-          queue.Enqueue n
-          count <- count + 1
+  System.Math.Ceiling(decimal count / 2M) |> int, markedGrid
 
+let countLine line =
+  let rec inner i crossingCount count =
+    match line |> Array.tryItem i with
+    | None -> count
+    | Some(LoopPipe x) ->
+      match x with
+      | NS _
+      | NW _
+      | NE _ -> inner (i + 1) (crossingCount + 1) count
+      | _ -> inner (i + 1) crossingCount count
+    | Some(Other _) when crossingCount % 2 <> 0 -> inner (i + 1) crossingCount (count + 1)
+    | Some(Other _) -> inner (i + 1) crossingCount count
 
-    System.Math.Ceiling(decimal count / 2M)
+  inner 0 0 0
 
-let partOne input =
-  let grid = input |> Parsing.parse
-  grid |> countLoopSize
+let data = input |> Parsing.parse
+let count, markedGrid = walkTheLoop data
 
-
-partOne input |> printfn "Part one: %A"
+count |> printfn "Part one: %i"
+markedGrid |> Array.sumBy countLine |> printfn "Part two: %i"
