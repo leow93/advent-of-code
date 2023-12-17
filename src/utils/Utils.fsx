2b15b64b127348beb9662cf3715086c2d1e90da5
leow93 <leowheelan@hotmail.co.uk>
2023-12-17 19:39:14 +0000

    utils changes

--- src/utils/Utils.fsx
+++ src/utils/Utils.fsx
@@ -5,10 +5,12 @@ module Input =
 
 
 module Strings =
-  let toCharArray (x: string) = x.ToCharArray() 
+  let toCharArray (x: string) = x.ToCharArray()
   let toStringArray = toCharArray >> Array.map string
   let split (sep: string) (s: string) = s.Split(sep)
-  
+
+  let replace (old: string) (x: string) (s: string) = s.Replace(old, x)
+
 module Maths =
   let rec private greatestCommonDivisor x y =
     match x with
