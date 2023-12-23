03b2ce97ce33f56e033ae564e834180980b45996
leow93 <leowheelan@hotmail.co.uk>
2023-12-23 21:33:44 +0000

    feat: day 21 part two but deathly slow

--- src/2023/21/main.fsx
+++ src/2023/21/main.fsx
@@ -5,11 +5,10 @@ open Utils
 
 let input = Input.readLines ()
 
-type Grid = {
-  rows: int
-  cols: int
-  grid: Dictionary<int * int, Cell>
-}
+type Grid =
+  { rows: int
+    cols: int
+    grid: Dictionary<int * int, Cell> }
 
 and Cell =
   | Start
@@ -29,16 +28,19 @@ module Parsing =
       | [] -> acc
 
     inner chs []
-    
+
   let private ofListGrid grid =
-      let result = Dictionary<int * int, Cell>()
-      let rows = grid |> List.length
-      let cols = grid[0] |> List.length
-      for i = 0 to (rows - 1) do
-        for j = 0 to (cols - 1) do
-          result.Add((i, j), grid[i][j])
-  
-      { rows = rows; cols = cols; grid = result }
+    let result = Dictionary<int * int, Cell>()
+    let rows = grid |> List.length
+    let cols = grid[0] |> List.length
+
+    for i = 0 to (rows - 1) do
+      for j = 0 to (cols - 1) do
+        result.Add((i, j), grid[i][j])
+
+    { rows = rows
+      cols = cols
+      grid = result }
 
 
   let parse (lines: string array) =
@@ -52,23 +54,25 @@ module InfiniteGrid =
     match state.grid.TryGetValue((i, j)) with
     | true, cell -> cell
     | false, _ ->
-      let normalisedI =
-        if i < 0 then state.rows + i else i
-        |> fun i -> i % state.rows
+      let normalisedI = i % state.rows |> fun x -> if x < 0 then state.rows + x else x
 
-      let normalisedJ =
-        if j < 0 then state.cols + j else j
-        |> fun j -> j % state.cols
+      let normalisedJ = j % state.cols |> fun x -> if x < 0 then state.cols + x else x
 
       state.grid[normalisedI, normalisedJ]
-  
+
+  let nextSteps (i, j) grid =
+    [ (i + 1, j); (i - 1, j); (i, j + 1); (i, j - 1) ]
+    |> List.choose (fun (x, y) -> if grid |> get (x, y) <> Rock then Some(x, y) else None)
+
+
 type Message = { count: int; coord: int * int }
 
 let findStart (grid: Grid) =
   let keys = grid.grid.Keys |> List.ofSeq
+
   let rec inner i =
     match keys |> List.tryItem i with
-    | Some k -> 
+    | Some k ->
       match grid.grid[k] with
       | Start -> Some k
       | _ -> inner (i + 1)
@@ -77,8 +81,8 @@ let findStart (grid: Grid) =
   inner 0
 
 let nextSteps (i, j) grid =
-  let rows = grid.rows 
-  let cols = grid.cols 
+  let rows = grid.rows
+  let cols = grid.cols
 
   [ (i + 1, j); (i - 1, j); (i, j + 1); (i, j - 1) ]
   |> List.choose (fun (x, y) ->
@@ -127,4 +131,27 @@ let partOne input =
 
   loop 64 Set.empty |> Set.count
 
+let partTwo input =
+  let grid = Parsing.parse input
+
+  let rec getNextSteps grid acc =
+    match acc |> Set.count with
+    | 0 -> getNextSteps grid (grid |> findStart |> Option.toList |> Set.ofList)
+    | _ ->
+      acc
+      |> Set.fold
+        (fun acc coord ->
+          let next = InfiniteGrid.nextSteps coord grid
+          Set.union acc (next |> Set.ofList))
+        Set.empty
+
+  let rec loop remainingSteps acc =
+    match remainingSteps with
+    | 0 -> acc
+    | n -> loop (n - 1) (getNextSteps grid acc)
+
+  let numberOfIterations = 500
+  loop numberOfIterations Set.empty |> Set.count
+
 input |> partOne |> printfn "Part one %d"
+input |> partTwo |> printfn "Part two: %d"
