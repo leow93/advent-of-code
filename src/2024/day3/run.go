b62472e5243064b7682f1cc2bc89ba05c848173b
leow93 <leowheelan@hotmail.co.uk>
2024-12-03 10:00:21 +0000

    refactor: simplify day3 a bit

--- src/2024/day3/run.go
+++ src/2024/day3/run.go
@@ -4,67 +4,62 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
-func partOne(input []string) (string, error) {
+func partOne(input string) (string, error) {
 	result := 0
-	// re := regexp.MustCompile(`(?m)mul(\(\d+),(\d+\))`)
-	// re := regexp.MustCompile(`mul\(\d+,\d+\)`)
 	re := regexp.MustCompile(`mul\((?P<X>\d+),(?P<Y>\d+)\)`)
-	for _, line := range input {
-		for _, match := range re.FindAllStringSubmatch(line, -1) {
-			if len(match) < 3 {
-				continue
-			}
-			x, err := strconv.Atoi(match[1])
-			if err != nil {
-				return "", err
-			}
-			y, err := strconv.Atoi(match[2])
-			if err != nil {
-				return "", err
-			}
+	for _, match := range re.FindAllStringSubmatch(input, -1) {
+		if len(match) < 3 {
+			continue
+		}
+		x, err := strconv.Atoi(match[1])
+		if err != nil {
+			return "", err
+		}
+		y, err := strconv.Atoi(match[2])
+		if err != nil {
+			return "", err
+		}
 
+		result += x * y
+	}
+	return fmt.Sprintf("%d", result), nil
+}
+
+func partTwo(input string) (string, error) {
+	enabled := true
+	result := 0
+	re := regexp.MustCompile(`(mul\((?P<X>\d+),(?P<Y>\d+)\))|(do\(\))|(don't\(\))`)
+
+	for _, match := range re.FindAllStringSubmatch(input, -1) {
+		if match[0] == "don't()" {
+			enabled = false
+		} else if match[0] == "do()" {
+			enabled = true
+		} else if enabled {
+			x, err := strconv.Atoi(match[2])
+			if err != nil {
+				return "", err
+			}
+			y, err := strconv.Atoi(match[3])
+			if err != nil {
+				return "", err
+			}
 			result += x * y
 		}
 	}
 	return fmt.Sprintf("%d", result), nil
 }
 
-func partTwo(input []string) (string, error) {
-	enabled := true
-	result := 0
-	re := regexp.MustCompile(`(?P<MUL>mul\((?P<X>\d+),(?P<Y>\d+)\))|(?P<DO>do\(\))|(?P<DONT>don't\(\))`)
-
-	for _, line := range input {
-		for _, match := range re.FindAllStringSubmatch(line, -1) {
-			if match[0] == "don't()" {
-				enabled = false
-			} else if match[0] == "do()" {
-				enabled = true
-			} else if enabled {
-				x, err := strconv.Atoi(match[2])
-				if err != nil {
-					return "", err
-				}
-				y, err := strconv.Atoi(match[3])
-				if err != nil {
-					return "", err
-				}
-				result += x * y
-			}
-		}
-	}
-	return fmt.Sprintf("%d", result), nil
-}
-
 func Run(input []string) (string, string, error) {
-	one, err := partOne(input)
+	one, err := partOne(strings.Join(input, ""))
 	if err != nil {
 		return "", "", err
 	}
 
-	two, err := partTwo(input)
+	two, err := partTwo(strings.Join(input, ""))
 	if err != nil {
 		return "", "", err
 	}
