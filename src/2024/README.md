4c1e611e8e2e497fa9f2d9f2a72e53bfd4942bb8
leow93 <leowheelan@hotmail.co.uk>
2024-12-05 21:01:13 +0000

    make running simpler

---
+++ src/2024/README.md
@@ -0,0 +1,19 @@
+# 2024
+
+## Run all
+
+```sh
+go run cmd/main.go -day=all
+```
+
+## Run one day
+
+```sh
+go run cmd/main.go -day=day1
+```
+
+## Run test input
+
+```sh
+go run cmd/main.go -day=day1 -test=true
+```
--- src/2024/cmd/main.go
+++ src/2024/cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/leow93/advent-of-code/2024/day1"
 	"github.com/leow93/advent-of-code/2024/day2"
@@ -15,31 +16,61 @@ import (
 
 type Runner func(input []string) (string, string, error)
 
+var ds = []string{"day1", "day2", "day3", "day4", "day5"}
+
 var days = map[string]Runner{
-	"day1": day1.Run,
-	"day2": day2.Run,
-	"day3": day3.Run,
-	"day4": day4.Run,
-	"day5": day5.Run,
+	ds[0]: day1.Run,
+	ds[1]: day2.Run,
+	ds[2]: day3.Run,
+	ds[3]: day4.Run,
+	ds[4]: day5.Run,
 }
 
-func main() {
-	flag.Parse()
-	day := flag.Arg(0)
+func readFileInput(day string, test bool) ([]string, error) {
+	if test {
+		return input.ReadLines(fmt.Sprintf("./%s/test_input.txt", day))
+	}
+	return input.ReadLines(fmt.Sprintf("./%s/input.txt", day))
+}
+
+func runDay(day string, test bool) {
+	fmt.Printf("%s\n", day)
+	defer func() {
+		fmt.Printf("\n-------------------\n\n")
+	}()
+	data, err := readFileInput(day, test)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			fmt.Printf("file does not exist")
+			return
+		}
+		panic(err)
+	}
 	runner, ok := days[day]
 	if !ok {
 		panic(errors.New("day " + day + " not found"))
 	}
 
-	data, err := input.FromStdin()
-	if err != nil {
-		panic(err)
-	}
 	part1, part2, err := runner(data)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("Part I: %s\n", part1)
-	fmt.Printf("Part II: %s\n", part2)
+	fmt.Printf("Part II: %s", part2)
+}
+
+func main() {
+	day := flag.String("day", "day1", "-day=day4")
+	test := flag.Bool("test", false, "-test=true")
+	flag.Parse()
+
+	if *day == "all" {
+		for _, day := range ds {
+			runDay(day, *test)
+		}
+		return
+	}
+
+	runDay(*day, *test)
 }
