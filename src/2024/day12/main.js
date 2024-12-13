17034220ff3e8f4c5b5d89fd5274502c74bbffb4
leow93 <leowheelan@hotmail.co.uk>
2024-12-13 10:04:59 +0000

    pain: day 12 2024

---
+++ src/2024/day12/main.js
@@ -0,0 +1,157 @@
+const filterMap = require('../../utils/filterMap');
+const runner = require('../../utils/runner');
+
+const parse = (input) =>
+  filterMap(input.split('\n'), (line) =>
+    !line || line === '\n' ? null : line.split('')
+  );
+
+const tryCell = (grid, [i, j]) => grid[i]?.[j] ?? null;
+const deltas = [
+  [-1, 0],
+  [1, 0],
+  [0, 1],
+  [0, -1],
+];
+
+const addCoord = (a, b) => [a[0] + b[0], a[1] + b[1]];
+
+function neighbours(grid, coord) {
+  const result = [];
+  for (const d of deltas) {
+    const c = addCoord(coord, d);
+    const n = tryCell(grid, c);
+    if (n != null) {
+      result.push(c);
+    }
+  }
+  return result;
+}
+
+const getRegions = (grid) => {
+  const rows = grid.length;
+  const cols = grid[0].length;
+
+  const visited = Array.from({ length: rows }, () => Array(cols).fill(false));
+  const isValid = (x, y, char) => {
+    return (
+      x >= 0 &&
+      x < rows &&
+      y >= 0 &&
+      y < cols &&
+      !visited[x][y] &&
+      grid[x][y] === char
+    );
+  };
+
+  const dfs = (x, y, char, region) => {
+    visited[x][y] = true;
+    region.push([x, y]);
+    for (const [i, j] of neighbours(grid, [x, y])) {
+      if (isValid(i, j, char)) {
+        dfs(i, j, char, region);
+      }
+    }
+  };
+
+  const regions = [];
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (!visited[i][j]) {
+        const region = [];
+        dfs(i, j, grid[i][j], region);
+        regions.push(region);
+      }
+    }
+  }
+
+  return regions;
+};
+
+const countRegionalNeighbours = (grid, i, j, char) => {
+  return neighbours(grid, [i, j]).filter(([x, y]) => grid[x][y] === char)
+    .length;
+};
+
+const getPrice = (grid, region) => {
+  const area = region.length;
+  let perimeter = 0;
+
+  for (const [x, y] of region) {
+    perimeter += 4 - countRegionalNeighbours(grid, x, y, grid[x][y]);
+  }
+
+  return area * perimeter;
+};
+
+const getCornerSquares = (grid, region, row, col) => {
+  let corners = 0;
+
+  const isInRegion = ([x, y]) => {
+    for (const [i, j] of region) {
+      if (i === x && j === y) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  for (const [dx, dy] of [
+    [1, 1],
+    [1, -1],
+    [-1, 1],
+    [-1, -1],
+  ]) {
+    const diagonalRow = row + dx;
+    const diagonalCol = col + dy;
+    const neighbourOne = [row, diagonalCol];
+    const neighbourTwo = [diagonalRow, col];
+
+    if (isInRegion([diagonalRow, diagonalCol])) {
+      // The diagonal is part of the region. This can only be a corner piece if
+      // the neighbouring squares aren't part of the shape.
+      if (!isInRegion(neighbourOne) && !isInRegion(neighbourTwo)) {
+        corners++;
+      }
+    } else if (isInRegion(neighbourOne) === isInRegion(neighbourTwo)) {
+      // diagonal is not part of the shape,
+      // but either both neighbours are or neither of them are, so it's a corner
+      corners++;
+    }
+  }
+
+  return corners;
+};
+
+const getNumCormers = (grid, region) => {
+  return region.reduce(
+    (count, [x, y]) => count + getCornerSquares(grid, region, x, y),
+    0
+  );
+};
+
+const getDiscountedPrice = (grid, region) => {
+  const area = region.length;
+  const sides = getNumCormers(grid, region);
+  return area * sides;
+};
+
+const sum = (grid, pricing) => (result, region) =>
+  result + pricing(grid, region);
+
+const partOne = (grid) => {
+  const rs = getRegions(grid);
+  return rs.reduce(sum(grid, getPrice), 0);
+};
+const partTwo = (grid) => {
+  const rs = getRegions(grid);
+  return rs.reduce(sum(grid, getDiscountedPrice), 0);
+};
+
+module.exports = {
+  getRegions,
+  parse,
+};
+
+if (require.main === module) runner(parse, partOne, partTwo);
