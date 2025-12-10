1d3622bd13024c59483558b788f1dc0d2b3ac3da
leow93 <leowheelan@hotmail.co.uk>
2025-12-10 16:41:54 +0000

    track time of each part

--- src/utils/runner.js
+++ src/utils/runner.js
@@ -1,19 +1,20 @@
 const readFromStdin = require('./readFromStdin');
 
-const each =
-  (...fs) =>
-  (...args) =>
-    Promise.resolve().then(() => Promise.all(fs.map((f) => f(...args))));
-
-const tap = (f) => (x) => {
-  f(x);
-  return x;
+const withPerf = async (f) => {
+  const start = performance.now();
+  const result = await Promise.resolve().then(() => f());
+  const end = performance.now();
+  return { result, duration: end - start };
 };
-
-const runner = (parser, partOne, partTwo) =>
-  readFromStdin()
-    .then(parser)
-    .then(each(partOne, partTwo))
-    .then(tap(console.log));
-
+const runner = async (parser, partOne, partTwo) => {
+  const input = await readFromStdin();
+  const data = parser(input);
+  const p1 = await withPerf(() => partOne(data, input));
+  console.log('Part I:', p1.result);
+  console.log(`(${p1.duration} ms)`);
+  const p2 = await withPerf(() => partTwo(data, input));
+  console.log('Part II:', p2.result);
+  console.log(`(${p2.duration} ms)`);
+  return [p1, p2];
+};
 module.exports = runner;
